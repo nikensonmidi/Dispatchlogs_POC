6eb2dd22cebbe038d00ccaf3c3bc29abe0de357c
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2020-08-12 16:48:04 -0400

    feat(interceptor): added interceptor

---
+++ NativeScript_DotNet/ClientApp/src/app/services/cache.interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { tap  } from 'rxjs/operators';
+import { CacheService } from './cache.service';
+
+export class CacheInterceptor implements HttpInterceptor {
+  constructor(private _cacheService: CacheService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      const cacheResponse: HttpResponse<any> = this._cacheService.getItem(req.url);
+      if (cacheResponse) { return of(cacheResponse); }
+
+  // if no response found in cache, go fetch it from server and add to cache
+return next.handle(req).pipe(
+tap( event => {
+if ( event instanceof HttpResponse) {
+  this._cacheService.additem(req.url, event);
+}
+
+})
+);
+
+    }
+
+}
